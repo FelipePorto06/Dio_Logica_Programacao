08fee8aef7ed13dd5f2356f69f48f692de420c5e
FelipePorto06 <felipeporto0606@gmail.com>
2024-11-17 06:15:56 -0300

    Teste

---
+++ 24_desafio_nivel_heroi.js
@@ -0,0 +1,34 @@
+let nomeHeroi = "Felipe"
+let xpHeroi = 23423423423
+
+if (xpHeroi < 1000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de ferro")
+}
+
+if (xpHeroi > 1001 && xpHeroi < 2000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Bronze")
+}
+
+if (xpHeroi > 2001 && xpHeroi < 5000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Prata")
+}
+
+if (xpHeroi > 5001 && xpHeroi < 7000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Ouro")
+}
+
+if (xpHeroi > 7001 && xpHeroi < 8000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Platina")
+}
+
+if (xpHeroi > 8001 && xpHeroi < 9000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Ascendente")
+}
+
+if (xpHeroi > 9001 && xpHeroi < 10000){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Imortal")
+}
+
+else if(xpHeroi > 10001){
+    console.log("O Héroi de nome " + nomeHeroi + " está no nivel de Radiante")
+}
