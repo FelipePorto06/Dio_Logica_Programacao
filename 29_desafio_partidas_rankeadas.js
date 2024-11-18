433e4deef05a3303c7b441ac3b067d7439135c82
FelipePorto06 <felipeporto0606@gmail.com>
2024-11-18 10:28:43 -0300

    Atualização do Desafio

--- 29_desafio_partidas_rankeadas.js
+++ 29_desafio_partidas_rankeadas.js
@@ -1,35 +1,29 @@
-function playerRank(vitorias, derrotas){
-    let rank = vitorias - derrotas
-    return rank
+function playerRank(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    return saldo;
 }
 
-let nivel = playerRank(10, 5)
+function partidasRankeadas(vitorias, derrotas) {
+    let saldo = playerRank(vitorias, derrotas);
+    let nivel;
 
-function partidasRankeadas(){
-    if(playerRank > 10){
-        playerRank = "Ferro"
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias > 100) {
+        nivel = "Imortal";
     }
-    if(nivel > 9 && nivel <= 20){
-        nivel = "Bronze"
-    }
-    if(nivel > 20 && nivel <= 50){
-        nivel = "Prata"
-    }
-    if(nivel > 50 && nivel <= 80){
-        nivel = "Ouro"
-    }
-    if(nivel > 80 && nivel <= 90){
-        nivel = "Diamante"
-    }
-    if(nivel > 90 && nivel <= 100){
-        nivel = "Lendário"
-    }
-    if(nivel > 100){
-        nivel = "Imortal"
-    }
-    return nivel
+    return { saldo, nivel };
 }
 
-console.log(nivel)
-
-
+let resultado = partidasRankeadas(55, 20);
+console.log(`O Herói tem um saldo de ${resultado.saldo} e está no nível ${resultado.nivel}.`);
